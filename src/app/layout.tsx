be4764b1dcc17464f89b4a55b92760356f017929
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-15 11:24:26 +0100

    add background effect for top of page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,23 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import BgEffect from '@/components/BgEffect';
+import './globals.css';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Liam Sutton | Portfolio",
-  description: "Showcase of Liam's skills, projects, and experience as a full-stack developer. Explore web development expertise in React, Next.js, Node.js, and more.",
+  title: 'Liam Sutton | Portfolio',
+  description:
+    "Showcase of Liam's skills, projects, and experience as a full-stack developer. Explore web development expertise in React, Next.js, Node.js, and more.",
 };
 
 export default function RootLayout({
@@ -24,10 +26,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang='en'>
+      <body className={`${geistSans.variable} ${geistMono.variable} relative bg-gray-50 antialiased`}>
+        <BgEffect />
         {children}
       </body>
     </html>
---
+++ src/components/BgEffect.tsx
@@ -0,0 +1,8 @@
+export default function BgEffect() {
+  return (
+    <>
+      <div className='absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] sm:w-[50vw] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-0'></div>
+      <div className='absolute right-0 top-[-1rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] sm:w-[50vw]'></div>
+    </>
+  );
+}
