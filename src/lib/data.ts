2d30299e9f865ddcc9a2cbc18b4bff288862d109
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-17 13:13:36 +0100

    add projects data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,7 @@
+import SocialImg from '../../public/images/social-ai.png';
+import walletImg from '../../public/images/wallet-ai.png';
+import SupernovaImg from '../../public/images/supernova.png';
+
 export const skills = [
   'JavaScript',
   'TypeScript',
@@ -36,4 +40,28 @@ export const links = [
   { name: 'Skills', link: '#skills' },
   { name: 'Experience', link: '#experience' },
   { name: 'Contact', link: '#contact' },
+] as const;
+
+export const projects = [
+  {
+    name: 'Supernova Dental',
+    description:
+      'A modern, responsive website built for a dental practice, Supernova Dental showcases services, provides appointment scheduling, and offers an intuitive user experience. The site is optimized for performance and SEO, ensuring both functionality and visibility.',
+    tags: ['React', 'Next.js', 'Prisma', 'Tailwind CSS', 'Framer Motion', 'TypeScript'],
+    image: SupernovaImg,
+  },
+  {
+    name: 'Social Ai',
+    description:
+      'A web app designed to track and analyze crypto wallet activity. Users can monitor wallet balances, transactions, and performance trends over time, providing an in-depth view of their cryptocurrency portfolio.',
+    tags: ['React', 'Next.js', 'NodeJs', 'Postgres', 'Tailwind CSS', 'TypeScript'],
+    image: SocialImg,
+  },
+  {
+    name: 'Wallet Ai',
+    description:
+      'A modern, responsive website built for a dental practice, Supernova Dental showcases services, provides appointment scheduling, and offers an intuitive user experience. The site is optimized for performance and SEO, ensuring both functionality and visibility.',
+    tags: ['React', 'Next.js', 'Prisma', 'Tailwind CSS', 'TypeScript'],
+    image: walletImg,
+  },
 ] as const;
