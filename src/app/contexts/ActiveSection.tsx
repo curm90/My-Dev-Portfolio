cd981441b3768bb69831f759219b128cc636c6db
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-17 17:48:51 +0100

    add active section context and custom consumer hook

---
+++ src/app/contexts/ActiveSection.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { createContext, useContext, useState } from 'react';
+import { links } from '@/lib/data';
+
+type TSectionName = (typeof links)[number]['name'];
+type TActiveSectionContextProvider = {
+  children: React.ReactNode;
+};
+type ActiveSectionContextType = {
+  activeTab: TSectionName;
+  setActiveTab: React.Dispatch<React.SetStateAction<TSectionName>>;
+};
+
+export const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null);
+
+export default function ActiveSectionContextProvider({ children }: TActiveSectionContextProvider) {
+  const [activeTab, setActiveTab] = useState<TSectionName>('Home');
+
+  return (
+    <ActiveSectionContext.Provider value={{ activeTab, setActiveTab }}>
+      {children}
+    </ActiveSectionContext.Provider>
+  );
+}
+
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
+
+  if (context === null) {
+    throw new Error('useActiveSection must be used within an ActiveSectionContextProvider');
+  }
+
+  return context;
+}
