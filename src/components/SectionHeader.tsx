0303a3228cd0a1c904df309ead270aa695312c86
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-17 13:14:14 +0100

    center header by default

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,3 +1,5 @@
 export default function SectionHeader({ header }: { header: string }) {
-  return <h2 className='mb-8 text-3xl font-medium capitalize text-gray-950'>{header}</h2>;
+  return (
+    <h2 className='mb-8 text-center text-3xl font-medium capitalize text-gray-950'>{header}</h2>
+  );
 }
