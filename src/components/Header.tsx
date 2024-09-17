6f1f2310b615127324c7863624a2522504f2b364
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-17 17:13:13 +0100

    animate header link bg effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,45 @@
 'use client';
 
-import { links } from '@/lib/data';
-import { motion } from 'framer-motion';
 import Link from 'next/link';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import clsx from 'clsx';
+import { links } from '@/lib/data';
 
 export default function Header() {
+  const [activeTab, setActiveTab] = useState('Home');
+
   return (
     <header className='relative z-[999]'>
       <motion.div
         initial={{ y: -100, x: '-50%', opacity: 0 }}
         animate={{ y: 0, x: '-50%', opacity: 1 }}
-        className='fixed left-1/2 top-0 w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
-      >
-        <nav className='flex h-full items-center justify-center py-3 sm:py-0'>
-          <ul className='flex h-full w-full flex-wrap items-center justify-center gap-1 text-gray-500'>
-            {links.map((link) => (
-              <li key={link.link}>
-                <Link className='p-3 transition hover:text-gray-400' href={link.link}>
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </motion.div>
+        className='fixed left-1/2 top-0 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
+      ></motion.div>
+      <nav className='fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
+        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
+          {links.map((link) => (
+            <motion.li className='relative flex h-3/4 items-center justify-center' key={link.link}>
+              <Link
+                onClick={() => setActiveTab(link.name)}
+                className={clsx('p-3 transition hover:text-gray-900', {
+                  'text-gray-900': activeTab === link.name,
+                })}
+                href={link.link}
+              >
+                {link.name}
+                {link.name === activeTab && (
+                  <motion.span
+                    className='absolute inset-0 -z-10 rounded-full bg-gray-100'
+                    layoutId='activeTab'
+                    transition={{ type: 'spring', stiffness: 380, damping: 30 }}
+                  ></motion.span>
+                )}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
