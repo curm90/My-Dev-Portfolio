e02c37c06bbb135bcae8bc6b7b08afb9f3be61be
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-18 20:49:35 +0100

    fix header animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,16 +19,21 @@ export default function Header() {
       <nav className='fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
         <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
           {links.map((link) => (
-            <motion.li className='relative flex h-3/4 items-center justify-center' key={link.link}>
+            <motion.li
+              className='relative flex h-3/4 items-center justify-center'
+              key={link.link}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
               <Link
                 onClick={() => {
                   setActiveTab(link.name);
                   setTimeOfLastClick(Date.now());
                 }}
                 className={clsx(
-                  'p-3 transition hover:text-gray-900 dark:text-gray-500 dark:hover:text-gray-300',
+                  'flex w-full items-center justify-center p-3 transition hover:text-gray-950 dark:text-gray-500 dark:hover:text-gray-300',
                   {
-                    'text-gray-900 dark:text-gray-200': activeTab === link.name,
+                    'text-gray-950 dark:text-gray-200': activeTab === link.name,
                   }
                 )}
                 href={link.link}
