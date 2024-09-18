b610bbd60f88e993e465f8f3d4c92a01d20db94b
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-18 15:04:22 +0100

    extract submit button into own component so we can keep track off form status and show loader

---
+++ src/components/SubmitBtn.tsx
@@ -0,0 +1,23 @@
+import { useFormStatus } from 'react-dom';
+import { FaPaperPlane } from 'react-icons/fa6';
+
+export default function SubmitBtn() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className='group flex h-[3rem] w-[8rem] items-center justify-center gap-2 rounded-full bg-gray-900 text-white outline transition-all hover:scale-105 hover:bg-gray-800 focus:scale-105 active:scale-100 disabled:scale-100 disabled:bg-opacity-65'
+      type='submit'
+      disabled={pending}
+    >
+      {pending ? (
+        <div className='h-5 w-5 animate-spin rounded-full border-b-2 border-white'></div>
+      ) : (
+        <>
+          Submit
+          <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:-translate-y-1 group-hover:translate-x-1' />
+        </>
+      )}
+    </button>
+  );
+}
