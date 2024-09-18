5751e33c251b9fbc0339e93962599d30b96264f7
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-18 10:16:15 +0100

    create custom hook to manage header tabs animation on scroll

---
+++ src/app/hooks/useSectionInView.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from 'react';
+import { useInView } from 'framer-motion';
+import { useActiveSectionContext } from '../contexts/ActiveSection';
+import type { TSectionName } from '@/types/Section';
+
+export default function useSectionView(sectionName: TSectionName = 'Home', amount = 0.75) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount });
+  const { setActiveTab, timeOfLastClick } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (isInView && Date.now() - timeOfLastClick > 1000) {
+      setActiveTab(sectionName);
+    }
+  }, [isInView, setActiveTab, timeOfLastClick, sectionName]);
+
+  return { ref };
+}
