f86aa9ebed3163657487cc95bb89c96ad0928aa3
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-18 14:34:18 +0100

    add helper functions to validate form inputs and handle errors

---
+++ src/lib/utils.ts
@@ -0,0 +1,23 @@
+export function validateEmailDataString(value: unknown, maxLength: number) {
+  if (!value || typeof value !== 'string' || value.length > maxLength) {
+    return false;
+  }
+
+  return true;
+}
+
+export function getErrorMessage(error: unknown): string {
+  let message = '';
+
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === 'object' && 'message' in error) {
+    message = String(error.message);
+  } else if (typeof error === 'string') {
+    message = error;
+  } else {
+    message = 'Something went wrong';
+  }
+
+  return message;
+}
