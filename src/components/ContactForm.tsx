a244a908697aa5a36f9cc4e45a99f7320c4e6723
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-18 12:56:14 +0100

    animate contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,9 +1,24 @@
+'use client';
+
+import { motion } from 'framer-motion';
 import { FaPaperPlane } from 'react-icons/fa6';
+import useSectionView from '@/app/hooks/useSectionInView';
+import { sendEmail } from '@/app/actions/contactForm';
 import SectionHeader from './SectionHeader';
 
 export default function ContactForm() {
+  const { ref } = useSectionView('Contact');
+
   return (
-    <section id='contact' className='w-[min(100%, 38rem)] mb-28 text-center sm:mb-40'>
+    <motion.section
+      ref={ref}
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+      id='contact'
+      className='w-[min(100%, 38rem)] mb-28 text-center sm:mb-40'
+    >
       <SectionHeader header='Contact me' />
       <p className='-mt-6 text-gray-700'>
         Please contact me directly at{' '}
@@ -12,16 +27,22 @@ export default function ContactForm() {
         </a>{' '}
         or through this form
       </p>
-      <form className='mt-10 flex flex-col'>
+      <form className='mt-10 flex flex-col' action={sendEmail}>
         <input
+          name='email'
           type='email'
           className='borderBlack h-14 rounded-lg p-4'
           placeholder='Your email...'
+          required
+          maxLength={100}
         />
         <textarea
+          name='message'
           placeholder='Your message...'
           className='borderBlack my-3 h-52 rounded-lg p-4'
-        ></textarea>
+          required
+          maxLength={100}
+        />
         <button
           className='group flex h-[3rem] w-[8rem] items-center justify-center gap-2 rounded-full bg-gray-900 text-white outline transition-all hover:scale-105 hover:bg-gray-800 focus:scale-105 active:scale-100'
           type='submit'
@@ -30,6 +51,6 @@ export default function ContactForm() {
           <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:-translate-y-1 group-hover:translate-x-1' />
         </button>
       </form>
-    </section>
+    </motion.section>
   );
 }
