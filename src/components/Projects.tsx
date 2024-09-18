f5a080d151325b2f5ec616beb864b233b49fba1b
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-18 10:37:40 +0100

    add bottom margin to projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,10 +7,10 @@ import Project from './Project';
 import useSectionView from '@/app/hooks/useSectionInView';
 
 export default function Projects() {
-  const { ref } = useSectionView('Projects', 0.25);
+  const { ref } = useSectionView('Projects', 0.5);
 
   return (
-    <section ref={ref} id='projects' className='mb-8 scroll-mt-28'>
+    <section ref={ref} id='projects' className='mb-28 scroll-mt-28'>
       <SectionHeader header='My projects' />
       <div className='flex flex-col gap-6'>
         {projects.map((project) => (
